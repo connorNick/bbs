41401b148e0418629370b91ff2be69481d9e4dfe
connorNick <550145191@qq.com>
2016-12-04 21:40:35 +0800

    更改

---
+++ bbs/bbs-domain/src/main/java/com/shareniu/bbs/domain/User.java
@@ -0,0 +1,153 @@
+package com.shareniu.bbs.domain;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private Integer replyCount;
+
+    private Integer topicCount;
+
+    private String sex;
+
+    private String username;
+
+    private String password;
+
+    private String lastLogionIp;
+
+    private Date lastLoginTime;
+
+    private Date registerTime;
+
+    private String email;
+
+    private Integer point;
+
+    private Integer status;
+
+    private String salt;
+    private String credentialsSalt;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getReplyCount() {
+        return replyCount;
+    }
+
+    public void setReplyCount(Integer replyCount) {
+        this.replyCount = replyCount;
+    }
+
+    public Integer getTopicCount() {
+        return topicCount;
+    }
+
+    public void setTopicCount(Integer topicCount) {
+        this.topicCount = topicCount;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getLastLogionIp() {
+        return lastLogionIp;
+    }
+
+    public void setLastLogionIp(String lastLogionIp) {
+        this.lastLogionIp = lastLogionIp == null ? null : lastLogionIp.trim();
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public Date getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(Date registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getCredentialsSalt() {
+        return credentialsSalt;
+    }
+
+    public void setCredentialsSalt(String credentialsSalt) {
+        this.credentialsSalt = credentialsSalt;
+    }
+}
